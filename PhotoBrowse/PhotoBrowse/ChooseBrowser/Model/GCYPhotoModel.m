4c88f22bb7649851122cbc65a2d934637c6ac310
gaochongyang <gaochongyang@enmonster.com>
2018-06-28 18:24:49 +0800

    model

--- PhotoBrowse/PhotoBrowse/ChooseBrowser/Model/GCYPhotoModel.m
+++ PhotoBrowse/PhotoBrowse/ChooseBrowser/Model/GCYPhotoModel.m
@@ -7,6 +7,7 @@
 //
 
 #import "GCYPhotoModel.h"
+#import <objc/runtime.h>
 
 const char * kThumbImageKey = "kThumbImageKey";//缩略图
 const char * kFullScreenImageKey = "kFullScreenImageKey";//屏幕大小图
@@ -28,10 +29,33 @@ const char * kOriginalImageSize = "kOriginalImageSize";//原图大小
 }
 
 - (NSString *)videoTime {
-    return nil;
+    NSInteger time = (NSInteger)self.phAsset.duration;
+    NSInteger minute = time / 60;
+    CGFloat second = time % 60;
+    return [NSString stringWithFormat:@"%zd:%.2f",minute,second];
 }
 
 - (void)thumbImageWithBlock:(GetThumbImageBlock)GetThumbImageBlock {
+    //取出关联对象 所关联的值
+    UIImage *image = objc_getAssociatedObject(self, kThumbImageKey);
+    if (image != nil) {
+        GetThumbImageBlock(image);
+        return;
+    }
+    
+    CGFloat itemWH = (SCREENWIDTH - (4+1)*KMARGIN*0.5)/4;
+    CGFloat screenScale = [UIScreen mainScreen].scale;
+    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+    options.deliveryMode = PHImageRequestOptionsDeliveryModeOpportunistic;
+    
+    [[GCYPhotoModel sharedPHImageManager] requestImageForAsset:self.phAsset
+                                                    targetSize:CGSizeMake(itemWH*screenScale, itemWH*screenScale)
+                                                   contentMode:PHImageContentModeDefault
+                                                       options:options
+                                                 resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
+                                                    // 此处设置关联对象
+                                                     objc_setAssociatedObject(self, kThumbImageKey, result, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+                                                 }];
     
 }
 
@@ -41,7 +65,14 @@ const char * kOriginalImageSize = "kOriginalImageSize";//原图大小
 #pragma mark -- private
 
 - (void)getOriginalImageSizeWithAsset:(PHAsset *)phAsset {
-
+    [[GCYPhotoModel sharedPHImageManager] requestImageDataForAsset:phAsset
+                                                           options:nil
+                                                     resultHandler:^(NSData * _Nullable imageData, NSString * _Nullable dataUTI, UIImageOrientation orientation, NSDictionary * _Nullable info) {
+                                                         CGFloat dataSize = imageData.length/(1024*1024.0);
+                                                         objc_setAssociatedObject(self, kOriginalImageSize, [NSString stringWithFormat:@"%f", dataSize], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+                                                         objc_setAssociatedObject(self, kOriginalImageData, imageData, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+                                                         objc_setAssociatedObject(self, kPHImageFileURLKey, [info objectForKey:@"PHImageFileURLKey"], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+                                                     }];
 }
 - (void)setPhAsset:(PHAsset *)phAsset {
     _phAsset = phAsset;
